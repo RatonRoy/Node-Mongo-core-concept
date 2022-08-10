965014f489914166cdae688849e5de845ad8a745
RatonRoy <royraton18@gmail.com>
2022-08-11 03:11:07 +0600

    setup files

---
+++ _step.js
@@ -0,0 +1,10 @@
+/******
+ * 1. for install the npm use npm init -y
+ * 2. For install the other dependencies use this command line  npm i express cors mongodb. 
+ * Cross-origin resources sharing use to connect the client side client request to the server side.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * ***** */
