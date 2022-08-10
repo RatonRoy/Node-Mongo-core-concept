6d20a45fd98d002adb37b0f09f27fd7c813a6586
RatonRoy <royraton18@gmail.com>
2022-08-11 05:51:53 +0600

    add some insertion option here

---
+++ _steps-mongodb-connection.js
@@ -0,0 +1,30 @@
+/******
+ * 1. import MongoClient, ServerApiVersion from mongodb [const { MongoClient, ServerApiVersion } = require('mongodb');]
+ * 
+ * 2. get uri and client from the mongodb database connection setting like this [
+    const uri = "mongodb+srv://crud-operation:<password>@curd.vrwqzgs.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+ ]
+ Here two thing is very important that replace the password by original database password 
+ * 
+ ***** Write this function to connect the data 
+ async function run() {
+	try {
+		await client.connect();
+		const userCollection = client.db('databaseName').collection('collectionName');
+		const demoObject = {
+			name: 'Mohona Nodi', email: 'mohonanodi@gmail.com'
+		}
+		const result = await userCollection.insertOne(demoObject);
+		console.log(`A document was inserted with the _id: ${result.insertedId}`);
+	}
+	finally {
+		// await client.close();
+		this is comment out because I want to use the database insertion again and again after one insertion complete. 
+	}
+}
+
+run().catch(console.dir);
+ * 
+ * ********* */
--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require('express');
-const cors = require(cors);
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000; 
 
+// user name : crud-operation
+// password : iUBZuAo1pIIgIpxF
 
-// use the middleware 
+// use the middle ware 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = "mongodb+srv://crud-operation:iUBZuAo1pIIgIpxF@curd.vrwqzgs.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+	try {
+		await client.connect();
+		const userCollection = client.db('demoDatabase').collection('demoCollection');
+		const demoObject = {
+			name: 'Mohona Nodi', email: 'mohonanodi@gmail.com'
+		}
+		const result = await userCollection.insertOne(demoObject);
+		console.log(`A document was inserted with the _id: ${result.insertedId}`);
+	}
+	finally {
+		// await client.close();
+	}
+}
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
 	res.send("Running My Node Server for Curd operation");
